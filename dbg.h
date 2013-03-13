d06b46616ca23646ab149eaa212b874f32d904af
Matt Widmann <mattongit@gmail.com>
2013-03-13 16:26:02 -0400

    simplify debug header

--- dbg.h
+++ dbg.h
@@ -8,22 +8,20 @@
 
 // logging
 
-#define MESSAGE(Prefix, Message) Prefix ": " Message "\n"
-
 #ifdef NDEBUG
 #define DBG(Message, ...)
+#define ERROR(Message, ...) fprintf(stderr, "ERROR: " Message "%s%s%s\n", ##__VA_ARGS__, (errno == 0 ? "" : " (errno: "), (errno == 0 ? "" : strerror(errno)), (errno == 0 ? "" : ")")); errno = 0
 #else
-#define DBG(Message, ...) fprintf(stderr, MESSAGE("DEBUG", Message " (%s:%d)"), ##__VA_ARGS__, __FILE__, __LINE__)
+#define DBG(Message, ...) fprintf(stderr, "DEBUG: " Message " (%s:%d)\n", ##__VA_ARGS__, __FILE__, __LINE__)
+#define ERROR(Message, ...) fprintf(stderr, "ERROR: " Message " (%s:%d%s%s)\n", ##__VA_ARGS__, __FILE__, __LINE__, (errno == 0 ? "" : ", errno: "), (errno == 0 ? "" : strerror(errno))); errno = 0
 #endif
-
-#define ERROR(Message, ...) fprintf(stderr, MESSAGE("ERROR", Message " (%s:%d%s%s)"), ##__VA_ARGS__, __FILE__, __LINE__, (errno == 0 ? "" : ", errno: "), (errno == 0 ? "" : strerror(errno))); errno = 0
 
 // assertions
 
-#define CHECK(Condition, Action, Message, ...) if (!(Condition)) { ERROR((Message), ##__VA_ARGS__); Action }
-#define CHECK_DBG(Condition, Message, ...) if (!(Condition)) { DBG((Message), ##__VA_ARGS__); }
+#define CHECK(Condition, Action, Message, ...) if (!(Condition)) { ERROR(Message, ##__VA_ARGS__); Action }
+#define CHECK_DBG(Condition, Message, ...) if (!(Condition)) { DBG(Message, ##__VA_ARGS__); }
 
-#define CHECK_EXIT(Condition, Message, ...) CHECK((Condition), exit(EXIT_FAILURE);, (Message), ##__VA_ARGS__)
+#define CHECK_EXIT(Condition, Message, ...) CHECK((Condition), exit(EXIT_FAILURE);, Message, ##__VA_ARGS__)
 #define CHECK_MEM(Condition) CHECK_EXIT((Condition), "out of memory")
 
 #endif
